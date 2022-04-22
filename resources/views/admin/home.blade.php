f5d91cfbd3bfc9b61f4c6264a09ea2832d03abd0
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-04-22 03:14:23 -0300

    General structure for dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -12,5 +12,51 @@
 
 
 @section('content')
-    ...
+    <div class="row">
+        <div class="col-md-3">
+            <div class="small-box bg-primary">
+                <div class="inner">
+                    <h3>R$50000,00</h3>
+                    <p>Faturamento</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-hand-holding-usd"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>R$40000,00</h3>
+                    <p>Cobrado</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-hand-holding-usd"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>R$50000,00</h3>
+                    <p>Vencido</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-hand-holding-usd"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>R$50000,00</h3>
+                    <p>Recebido</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-hand-holding-usd"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
